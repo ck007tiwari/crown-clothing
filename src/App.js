8506644b450deb27a3119c08196df9290582d5f6
ck007tiwari <chandrakishort20@gmail.com>
2021-12-06 12:35:27 +0530

    finishing signpu

--- src/App.js
+++ src/App.js
@@ -37,10 +37,8 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {
-            console.log("printing the State", this.state);
-          })
-        })
+          });
+        });
         
       } else {
         this.setState({ currentUser: userAuth });
